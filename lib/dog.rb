4472da0444946a1ec40121aaf4ddd29df9c23576
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-26 20:21:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,6 +74,7 @@ attr_accessor :name, :breed, :id
       dog_data = dog[0]
       new_dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
+      hash = {name: name, breed: breed}
       new_dog = self.create(name: name, breed: breed)
     end
     new_dog
