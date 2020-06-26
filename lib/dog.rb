7473a042e6f4f71ba9b110974893a3c737feaf45
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-26 19:46:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,14 @@ attr_accessor :name, :breed, :id
   end
 
   def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute.sql
 
   end
 
